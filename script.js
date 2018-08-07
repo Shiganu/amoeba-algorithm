3fc3818ea782bf9ecd72ee57e38d53e71991d6c2
MB2017 <minecraftredstonemaniak@gmail.com>
2018-08-07 19:43:36 +0200

    Add comments to script.js

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 
+// For generating algorithm points close to each other
 let triCenter = new Vec2(random(0, width), random(0, height));
-let triMaxDist = 150;
+let triMaxDist = 200;
 
-let v1 = new Vec2(triCenter.x + random(0, triMaxDist), triCenter.y + random(0, triMaxDist));
-let v2 = new Vec2(triCenter.x + random(0, triMaxDist), triCenter.y + random(0, triMaxDist));
-let v3 = new Vec2(triCenter.x + random(0, triMaxDist), triCenter.y + random(0, triMaxDist));
+// Algorithm points
+let v1 = new Vec2(
+	triCenter.x + random(0, triMaxDist),
+	triCenter.y + random(0, triMaxDist)
+);
+let v2 = new Vec2(
+	triCenter.x + random(0, triMaxDist),
+	triCenter.y + random(0, triMaxDist)
+);
+let v3 = new Vec2(
+	triCenter.x + random(0, triMaxDist),
+	triCenter.y + random(0, triMaxDist)
+);
+
+// Target point
 let t = new Vec2(random(0, width), random(0, height));
 
+// Calculating distances
 let v1dist = dist(v1, t);
 let v2dist = dist(v2, t);
 let v3dist = dist(v3, t);
 
+// Worst, best and other initialization
 let w, b, o;
 
+// Checking which vector is the best, worst and other
 if(v1dist < v2dist && v1dist < v3dist)
 {
 	b = v1;
@@ -56,28 +72,31 @@ else
 	}
 }
 
-
 function main()
 {
+	// Fill the screen black
 	c.fillStyle = "#000000";
 	c.fillRect(0, 0, width, height);
 
-	
+	// Algorithm start
+	// Algorithm end
 
+	// Draw the algorithm triangle
 	c.strokeStyle = "#ffffff";
 	line(w.x, w.y, o.x, o.y);
 	line(w.x, w.y, b.x, b.y);
 	line(b.x, b.y, o.x, o.y);
 
+	// Color the points
 	c.fillStyle = "#ff0000";
 	fillCircle(w.x, w.y, 8);
 	c.fillStyle = "#ffff00";
 	fillCircle(o.x, o.y, 8);
 	c.fillStyle = "#00ff00";
 	fillCircle(b.x, b.y, 8);
-
 	c.fillStyle = "#ffffff";
 	fillCircle(t.x, t.y, 8);
 }
 
+// Main loop
 let loop = setInterval(main, 50/3);
