57edd8c1fb9a463a5f43731f183734910c9e2d9d
MB2017 <minecraftredstonemaniak@gmail.com>
2018-08-07 20:32:27 +0200

    Implementing algorithm

--- script.js
+++ script.js
@@ -41,19 +41,19 @@ if(v1dist < v2dist && v1dist < v3dist)
 {
 	b = v1;
 	if(v2dist < v3dist) { o = v2; w = v3; }
-	else { o = v3; w = v2; }
+	else                { o = v3; w = v2; }
 }
 else if(v2dist < v1dist && v2dist < v3dist)
 {
 	b = v2;
 	if(v1dist < v3dist) { o = v1; w = v3; }
-	else { o = v3; w = v1; }
+	else                { o = v3; w = v1; }
 }
 else
 {
 	b = v3;
 	if(v1dist < v2dist) { o = v1; w = v2; }
-	else { o = v2; w = v1; }
+	else                { o = v2; w = v1; }
 }
 
 function main()
@@ -63,6 +63,52 @@ function main()
 	c.fillRect(0, 0, width, height);
 
 	// Algorithm start
+
+	// Calculating centroid
+	let centroid = o.get();
+	centroid.sub(b);
+	centroid.mult(0.5);
+	centroid.add(b);
+
+	// Calculating expanded
+	let expanded = centroid.get();
+	expanded.sub(w);
+	expanded.mult(2);
+	expanded.add(w);
+
+	c.fillStyle = "#ffffff";
+	fillCircle(expanded.x, expanded.y, 8);
+
+	// New points
+	let v1 = o;
+	let v2 = b;
+	let v3 = expanded;
+
+	// Calculating distances
+	let v1dist = dist(v1, t);
+	let v2dist = dist(v2, t);
+	let v3dist = dist(v3, t);
+
+	// Checking which vector is the best, worst and other
+	if(v1dist < v2dist && v1dist < v3dist)
+	{
+		b = v1;
+		if(v2dist < v3dist) { o = v2; w = v3; }
+		else                { o = v3; w = v2; }
+	}
+	else if(v2dist < v1dist && v2dist < v3dist)
+	{
+		b = v2;
+		if(v1dist < v3dist) { o = v1; w = v3; }
+		else                { o = v3; w = v1; }
+	}
+	else
+	{
+		b = v3;
+		if(v1dist < v2dist) { o = v1; w = v2; }
+		else                { o = v2; w = v1; }
+	}
+
 	// Algorithm end
 
 	// Draw the algorithm triangle
@@ -83,4 +129,4 @@ function main()
 }
 
 // Main loop
-let loop = setInterval(main, 50/3);
+let loop = setInterval(main, 1000/10);
